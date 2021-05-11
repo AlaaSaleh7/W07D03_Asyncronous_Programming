865d955600ef7baf5201b3c0b4476eab037b5f5a
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-12 01:39:01 +0300

    practice

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const getPost = (id)=>{
         axios
         .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then((response)=>{
-            //console.log(response.data);
+            console.log(response.data);
             res.json(response.data)
         })
         .catch((err)=>{
@@ -51,9 +51,9 @@ getPost();
 const getPostAsync = async (data) => {
     try{
         const response = await axios.get(
-            "https://jsonplaceholder.typicode.com/posts"
+            `https://jsonplaceholder.typicode.com/${data}`
         );
-        //console.log(response.data);
+        console.log(response.data);
     } catch(err){
         throw err;
     }
@@ -79,17 +79,62 @@ const getPostAsync = async (data) => {
         if (err) throw err;
         console.log(`data.txt was copied to ${fileName}`);
       }
-      
       // destination.txt will be created or overwritten by default.
       fs.copyFile('data.txt', fileName, callback);
       
       // By using COPYFILE_EXCL, the operation will fail if destination.txt exists.
-      fs.copyFile('source.txt', fileName, fs.constants.COPYFILE_EXCL, callback);
+      fs.copyFile('data.txt', fileName, fs.constants.COPYFILE_EXCL, callback);
   };
   copyFile();
 //--------------------------------------------------------------------------
 //q3
-
+const newPost = JSON.stringify({
+    title: "JavaScript Basics",
+    body: "This post contains information about javaScript ",
+    // the id of the user who is going to create the post
+    userId: 1,
+  });
+  
+  const createPost = (post) => {
+    // TODO: Your code here
+  };
+  createPost(newPost)
+//--------------------------------------------------------------------------
+//q4
+const newPost = JSON.stringify({
+    id: 1,
+    title: "Updated Title",
+    body: "Updated body",
+    userId: 1,
+  });
+  
+  const updatePost = (id, data) => {
+    // TODO: Your code here
+  };
+  
+  updatePost(1, newPost);
+//--------------------------------------------------------------------------
+//q5
+const getUsers = async (id) => {
+    try{
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        console.log(response.data);
+    } catch(err){
+        throw err;
+    }
+  };
+  getUsers();
+  fetch( `https://jsonplaceholder.typicode.com/posts/${id}`)
+  .then(response => response.json())
+  .then(json => console.log(json))
+//--------------------------------------------------------------------------
+//q6
+const saveUsers = () => {
+    // TODO: Your code here
+  };
+  saveUsers();
 //--------------------------------------------------------------------------
 // to sure that server work
 app.get("/",(req,res)=>{
